b3ed6745a523479ab29bb12a0ef378aa74a51480
TearGosling <teargoslingwaifu@gmail.com>
2023-11-08 13:54:58 -0600

    Add proposed "RWKV-6"

--- RWKV-v5/src/model.py
+++ RWKV-v5/src/model.py
@@ -181,6 +181,108 @@ class RWKV_TimeMix_RWKV5(MyModule):
         x = RUN_CUDA_RWKV5(B, T, C, H, r, k, v, w=self.time_decay, u=self.time_faaaa)
 
         return self.jit_func_2(x, g)
+    
+class RWKV_TimeMix_RWKV6(MyModule):
+    def __init__(self, args, layer_id):
+        super().__init__()
+        self.args = args
+        self.layer_id = layer_id
+
+        self.head_size = args.head_size_a
+        assert HEAD_SIZE == self.head_size # change HEAD_SIZE to match args.head_size_a
+        self.n_head = args.dim_att // self.head_size
+        assert args.dim_att % self.n_head == 0
+        self.head_size_divisor = args.head_size_divisor
+
+        with torch.no_grad():
+            ratio_0_to_1 = layer_id / (args.n_layer - 1)  # 0 to 1
+            ratio_1_to_almost0 = 1.0 - (layer_id / args.n_layer)  # 1 to ~0
+            ddd = torch.ones(1, 1, args.n_embd)
+            for i in range(args.n_embd):
+                ddd[0, 0, i] = i / args.n_embd
+
+            # fancy time_mix
+            self.time_mix_k = nn.Parameter(torch.pow(ddd, ratio_1_to_almost0))
+            self.time_mix_v = nn.Parameter(torch.pow(ddd, ratio_1_to_almost0) + 0.3 * ratio_0_to_1)
+            self.time_mix_r = nn.Parameter(torch.pow(ddd, 0.5 * ratio_1_to_almost0))
+            self.time_mix_g = nn.Parameter(torch.pow(ddd, 0.5 * ratio_1_to_almost0))
+
+            # fancier, data-dependant time_mix
+            self.time_mix_k_w1 = nn.Parameter(torch.empty(args.n_embd, args.time_mix_extra_dim).uniform_(-0.01, 0.01))
+            self.time_mix_k_w2 = nn.Parameter(torch.zeros(args.time_mix_extra_dim, args.n_embd))
+            self.time_mix_v_w1 = nn.Parameter(torch.empty(args.n_embd, args.time_mix_extra_dim).uniform_(-0.01, 0.01))
+            self.time_mix_v_w2 = nn.Parameter(torch.zeros(args.time_mix_extra_dim, args.n_embd))
+            self.time_mix_r_w1 = nn.Parameter(torch.empty(args.n_embd, args.time_mix_extra_dim).uniform_(-0.01, 0.01))
+            self.time_mix_r_w2 = nn.Parameter(torch.zeros(args.time_mix_extra_dim, args.n_embd))
+            self.time_mix_g_w1 = nn.Parameter(torch.empty(args.n_embd, args.time_mix_extra_dim).uniform_(-0.01, 0.01))
+            self.time_mix_g_w2 = nn.Parameter(torch.zeros(args.time_mix_extra_dim, args.n_embd))
+
+            # fancy time_decay
+            decay_speed = torch.ones(args.dim_att)
+            for n in range(args.dim_att):
+                decay_speed[n] = -6 + 5 * (n / (args.dim_att - 1)) ** (0.7 + 1.3 * ratio_0_to_1)
+            self.time_decay = nn.Parameter(decay_speed.reshape(self.n_head, self.head_size))
+            # print(layer_id, self.time_decay.flatten()[:3].cpu().numpy(), '...', self.time_decay.flatten()[-3:].cpu().numpy())
+
+            tmp = torch.zeros(args.dim_att)
+            for n in range(args.dim_att):
+                zigzag = ((n + 1) % 3 - 1) * 0.1
+                tmp[n] = ratio_0_to_1 * (1 - (n / (args.dim_att - 1))) + zigzag
+
+            self.time_faaaa = nn.Parameter(tmp.reshape(self.n_head, self.head_size))
+
+        self.time_shift = nn.ZeroPad2d((0, 0, 1, -1))
+        self.receptance = nn.Linear(args.n_embd, args.dim_att, bias=False)
+        self.key = nn.Linear(args.n_embd, args.dim_att, bias=False)
+
+        self.value = nn.Linear(args.n_embd, args.dim_att, bias=False)
+        self.output = nn.Linear(args.dim_att, args.n_embd, bias=False)
+        self.gate = nn.Linear(args.n_embd, args.dim_att, bias=False)
+        self.ln_x = nn.GroupNorm(self.n_head, args.dim_att)
+
+        self.gate_act_fn = ACT2CLS[args.gate_act_fn](args)
+
+    @MyFunction
+    def jit_func(self, x):
+        B, T, C = x.size()
+
+        # mix x depending on data
+        time_mix_k = self.time_mix_k.view(1,1,-1) + (x @ self.time_mix_k_w1) @ self.time_mix_k_w2
+        time_mix_v = self.time_mix_v.view(1,1,-1) + (x @ self.time_mix_v_w1) @ self.time_mix_v_w2
+        time_mix_r = self.time_mix_r.view(1,1,-1) + (x @ self.time_mix_r_w1) @ self.time_mix_r_w2
+        time_mix_g = self.time_mix_g.view(1,1,-1) + (x @ self.time_mix_g_w1) @ self.time_mix_g_w2
+
+        xx = self.time_shift(x) # Mix x with the previous timestep to produce xk, xv, xr
+        xk = x * time_mix_k + xx * (1 - time_mix_k)
+        xv = x * time_mix_v + xx * (1 - time_mix_v)
+        xr = x * time_mix_r + xx * (1 - time_mix_r)
+        xg = x * time_mix_g + xx * (1 - time_mix_g)
+
+        r = self.receptance(xr)
+        k = self.key(xk)
+        v = self.value(xv)
+        g = self.gate_act_fn(self.gate(xg))
+
+        return r, k, v, g
+
+    @MyFunction
+    def jit_func_2(self, x, g):
+        B, T, C = x.size()
+        x = x.view(B * T, C)
+        
+        x = self.ln_x(x / self.head_size_divisor).view(B, T, C)
+        x = self.output(x * g)
+        return x
+
+    def forward(self, x):
+        B, T, C = x.size()
+        H = self.n_head
+
+        r, k, v, g = self.jit_func(x)
+
+        x = RUN_CUDA_RWKV5(B, T, C, H, r, k, v, w=self.time_decay, u=self.time_faaaa)
+
+        return self.jit_func_2(x, g)
 
 ########################################################################################################
 
@@ -211,6 +313,46 @@ class RWKV_ChannelMix(MyModule):
         xx = self.time_shift(x)
         xk = x * self.time_mix_k + xx * (1 - self.time_mix_k)
         xr = x * self.time_mix_r + xx * (1 - self.time_mix_r)
+        k = self.key(xk)
+        k = self.ffn_act_fn(k)
+        kv = self.value(k)
+        return self.forget_gate(self.receptance(xr)) * kv
+    
+class RWKV_ChannelMix_RWKV6(MyModule):
+    def __init__(self, args, layer_id):
+        super().__init__()
+        self.args = args
+        self.layer_id = layer_id
+        self.time_shift = nn.ZeroPad2d((0, 0, 1, -1))
+
+        with torch.no_grad():  # fancy init of time_mix
+            ratio_1_to_almost0 = 1.0 - (layer_id / args.n_layer)  # 1 to ~0
+            ddd = torch.ones(1, 1, args.n_embd)
+            for i in range(args.n_embd):
+                ddd[0, 0, i] = i / args.n_embd
+            self.time_mix_k = nn.Parameter(torch.pow(ddd, ratio_1_to_almost0))
+            self.time_mix_r = nn.Parameter(torch.pow(ddd, ratio_1_to_almost0))
+
+            self.time_mix_k_w1 = nn.Parameter(torch.empty(args.n_embd, args.time_mix_extra_dim).uniform_(-0.01, 0.01))
+            self.time_mix_k_w2 = nn.Parameter(torch.zeros(args.time_mix_extra_dim, args.n_embd))
+            self.time_mix_r_w1 = nn.Parameter(torch.empty(args.n_embd, args.time_mix_extra_dim).uniform_(-0.01, 0.01))
+            self.time_mix_r_w2 = nn.Parameter(torch.zeros(args.time_mix_extra_dim, args.n_embd))
+        
+        self.key = nn.Linear(args.n_embd, args.dim_ffn, bias=False)
+        self.receptance = nn.Linear(args.n_embd, args.n_embd, bias=False)
+        self.value = nn.Linear(args.dim_ffn, args.n_embd, bias=False)
+
+        self.ffn_act_fn = ACT2CLS[args.ffn_act_fn](args)
+        self.forget_gate = ACT2CLS[args.ffn_forget_gate](args)
+
+    @MyFunction
+    def forward(self, x):
+        time_mix_k = self.time_mix_k.view(1,1,-1) + (x @ self.time_mix_k_w1) @ self.time_mix_k_w2
+        time_mix_r = self.time_mix_r.view(1,1,-1) + (x @ self.time_mix_r_w1) @ self.time_mix_r_w2
+
+        xx = self.time_shift(x)
+        xk = x * time_mix_k + xx * (1 - time_mix_k)
+        xr = x * time_mix_r + xx * (1 - time_mix_r)
         k = self.key(xk)
         k = self.ffn_act_fn(k)
         kv = self.value(k)
@@ -267,15 +409,23 @@ class Block(nn.Module):
                 self.pos_emb_x = nn.Parameter(torch.zeros((1,args.my_pos_emb,args.n_embd)))
                 self.pos_emb_y = nn.Parameter(torch.zeros((args.my_pos_emb,1,args.n_embd)))
 
-        if self.layer_id == 0 and self.args.pre_ffn > 0:
-            self.ffnPre = RWKV_ChannelMix(args, 0)
+        # determine specific classes
+        if 'v6' in os.environ["RWKV_MY_TESTING"]:
+            attn_class = RWKV_TimeMix_RWKV6
+            ffn_class = RWKV_ChannelMix_RWKV6
         else:
-            self.att = RWKV_TimeMix_RWKV5(args, layer_id)
+            attn_class = RWKV_TimeMix_RWKV5
+            ffn_class = RWKV_ChannelMix
+
+        if self.layer_id == 0 and self.args.pre_ffn > 0:
+            self.ffnPre = ffn_class(args, 0)
+        else:
+            self.att = attn_class(args, layer_id)
 
         if 'g' in os.environ["RWKV_MY_TESTING"]:
             self.ffn = MishGLU(args, layer_id)
         else:
-            self.ffn = RWKV_ChannelMix(args, layer_id)
+            self.ffn = ffn_class(args, layer_id)
         
         if args.tiny_att_dim > 0 and self.layer_id == args.tiny_att_layer:
             self.tiny_ln = nn.LayerNorm(args.n_embd)
